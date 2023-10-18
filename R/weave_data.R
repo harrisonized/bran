db38b3966669dce554021ddeb6afe79e8828517b
Harrison Wang <harrisonized@gmail.com>
2023-10-18 10:11:56 -0400

    calculate dates through R instead of relying on Transnetyx

--- R/weave_data.R
+++ R/weave_data.R
@@ -14,11 +14,11 @@ source(file.path(wd_, 'R', 'functions', 'df_tools.R'))  # read_excel_or_csv
 
 # args
 option_list = list(
-    make_option(c("-o", "--old-file"), default='data/family-tree/old_data.csv',
+    make_option(c("-i", "--initial-data"), default='data/family-tree/old_data.csv',
                 metavar='data/family-tree/old_data.csv',
                 type="character",help="template file where you listed your annnotations"),
 
-    make_option(c("-n", "--new-file"), default='data/family-tree/My_Mice.xlsx',
+    make_option(c("-n", "--new-data"), default='data/family-tree/My_Mice.xlsx',
                 metavar='data/family-tree/My_Mice.xlsx',
                 type="character",help="new file downloaded directly from transnetyx"),
 
@@ -41,28 +41,31 @@ log_print(paste('Script started at:', start_time))
 # Main
 
 # read data
-old_df = read_excel_or_csv(file.path(wd, opt[['old-file']]))
-old_df <- preprocessing(old_df)
-new_df = read_excel_or_csv(file.path(wd, opt[['new-file']]))
-new_df <- preprocessing(new_df, impute_missing_parents=FALSE)
+old_data = read_excel_or_csv(file.path(wd, opt[['initial-data']]))
+old_data <- preprocessing(old_data)
+new_data = read_excel_or_csv(file.path(wd, opt[['new-data']]))
+new_data <- preprocessing(new_data, impute_missing_parents=FALSE)
 
 # add missing mice
-new_df <- rbind(new_df, dplyr::anti_join(old_df, new_df, by='mouse_id'))  # add missing mice from old to new
-old_df <- rbind(old_df, dplyr::anti_join(new_df, old_df, by='mouse_id'))  # add new mice
+new_data <- rbind(new_data, dplyr::anti_join(old_data, new_data, by='mouse_id'))  # add missing mice from old to new
+old_data <- rbind(old_data, dplyr::anti_join(new_data, old_data, by='mouse_id'))  # add new mice
 
-# overwrite update_cols using data from new_df
+# overwrite update_cols using data from new_data
 update_cols = c('age', 'genotype', 'labels', 'cage_id', 'notes', 'rack', 'position')
 df <- left_join(
-    old_df[, c(items_in_a_not_b(colnames(old_df), update_cols))],
-    new_df[, c('mouse_id', update_cols)],
+    old_data[, c(items_in_a_not_b(colnames(old_data), update_cols))],
+    new_data[, c('mouse_id', update_cols)],
     by='mouse_id'
 )
 df <- preprocessing(df)
-df <- df[, colnames(old_df)]  # column order
+df <- df[, colnames(old_data)]  # reorder columns
+
+# update date instead of relying on Transnetyx
+df[['age']] <- as.integer(difftime(Sys.Date(), as.Date(df[['dob']], "%m/%d/%y")))
 
 # save
 if (!troubleshooting) {
-    directory = file.path(wd, dirname(opt[['old-file']]), 'troubleshooting')
+    directory = file.path(wd, dirname(opt[['initial-data']]), 'troubleshooting')
     if (!dir.exists(directory)) {
         dir.create(directory, recursive=TRUE)
     }
@@ -70,7 +73,7 @@ if (!troubleshooting) {
     # merged file
     filepath = file.path(
         directory,
-        paste0('_', tools::file_path_sans_ext(basename(opt[['old-file']])), '.csv')  # filename
+        paste0('_', tools::file_path_sans_ext(basename(opt[['initial-data']])), '.csv')  # filename
     )
     write.table(df, file = filepath, row.names = FALSE, sep = ',' )
 }
